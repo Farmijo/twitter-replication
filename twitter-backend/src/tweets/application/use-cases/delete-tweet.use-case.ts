c3c9c58c055ccadb5bd12103fcf976ec04035a79
Fran <farmijo16@gmail.com>
2025-10-29 16:41:53 +0100

    Delete tweet cleaunp

--- twitter-backend/src/tweets/application/use-cases/delete-tweet.use-case.ts
+++ twitter-backend/src/tweets/application/use-cases/delete-tweet.use-case.ts
@@ -6,7 +6,7 @@ import { TWEET_TOKENS } from '../tokens';
 
 export interface DeleteTweetCommand {
   tweetId: string;
-  userId: string; // Usuario que intenta eliminar
+  userId: string;
 }
 
 @Injectable()
@@ -17,22 +17,19 @@ export class DeleteTweetUseCase {
   ) {}
 
   async execute(command: DeleteTweetCommand): Promise<void> {
-  const tweetId = new TweetId(command.tweetId);
-  const authorId = AuthorId.fromString(command.userId);
+    const tweetId = new TweetId(command.tweetId);
+    const authorId = AuthorId.fromString(command.userId);
 
-    // Obtener el tweet
     const tweet = await this.tweetRepository.findById(tweetId);
     
     if (!tweet) {
       throw new NotFoundException(`Tweet with id ${command.tweetId} not found`);
     }
 
-    // Verificar que el usuario puede eliminar el tweet
-  if (!tweet.canBeDeletedBy(authorId)) {
+    if (!tweet.canBeDeletedBy(authorId)) {
       throw new Error('You can only delete your own tweets');
     }
 
-    // Eliminar el tweet
     await this.tweetRepository.delete(tweetId);
   }
 }
